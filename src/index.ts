11d050ea79fd4ecd0ff1e3bb2252a88e1890e8ee
Sebahattin <1030220637@erciyes.edu.tr>
2024-10-01 14:25:16 +0300

    file extensions and files changed
    new endpoints, new jwt etc.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { router } from './routes';
 
-// Load environment variables
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-console.log("port", PORT);
+
+
 app.use(express.json());
+app.use(cors());
+
 
 app.use(router);
 
-// Start the server
+
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Backend server running on http://localhost:${PORT}`);
 });

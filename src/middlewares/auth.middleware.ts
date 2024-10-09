d215b9d7a2fcfaff5dce4ddf2290405d1165514c
Fatmanur Ergen <fatmanurergen38@gmail.com>
2024-10-09 16:02:30 +0300

    middlewware updated

--- src/middlewares/auth.middleware.ts
+++ src/middlewares/auth.middleware.ts
@@ -1,12 +1,19 @@
-// src/middlewares/auth.middleware.ts
 import { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
 
-export const authMiddleware = (req: Request, res: Response, next: NextFunction): void => {
-  const { email, password } = req.body;
+export const authMiddleware = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const token = req.cookies.authToken;
 
-  if (!email || !password) {
-    res.status(400).json({ message: 'Email and password are required' });
-  } else {
-    next();
+  if (!token) {
+    res.status(401).json({ message: 'Access denied. No token provided.' });
+    return;
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET_KEY as string);
+    (req as any).user = decoded; // Tipin uyduğuna emin olduktan sonra req.user'a atama yapıyoruz
+    next(); // Bir sonraki middleware'e geçiliyor
+  } catch (err) {
+    res.status(400).json({ message: 'Invalid token' });
   }
 };
--- src/routes/questionRoutes.ts
+++ src/routes/questionRoutes.ts
@@ -1,19 +1,20 @@
 import express from 'express';
 import { createQuestionPackage, getQuestionPackages ,deleteQuestionPackage, updateQuestionPackage,getQuestionPackageById} from '../controllers/question-package.controller';
+import { authMiddleware } from '../middlewares/auth.middleware';
 
 const router = express.Router();
 
 // Soru paketi oluşturma route'u
-router.post('/', createQuestionPackage);
+router.post('/', authMiddleware, createQuestionPackage);
 
-router.get('/', getQuestionPackages);
+router.get('/', authMiddleware, getQuestionPackages);
 
 // ID'ye göre soru paketi getirme route'u
-router.get('/:id', getQuestionPackageById); // Yeni route
+router.get('/:id', authMiddleware, getQuestionPackageById); // Yeni route
 
-router.delete('/:id', deleteQuestionPackage);
+router.delete('/:id', authMiddleware, deleteQuestionPackage);
 
 // Soru paketi güncelleme
-router.put('/:id', updateQuestionPackage);
+router.put('/:id', authMiddleware, updateQuestionPackage);
 
 export default router;

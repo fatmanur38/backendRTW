7888ed6bfb4c91aa49c3bd1642039258050c46f3
Fatmanur Ergen <fatmanurergen38@gmail.com>
2024-11-06 14:15:01 +0300

    fixed2 upload middleware

--- src/middlewares/upload.middleware.ts
+++ src/middlewares/upload.middleware.ts
@@ -1,10 +1,9 @@
 // src/middlewares/upload.middleware.ts
-import multer, { StorageEngine } from 'multer';
+import multer from 'multer';
 import path from 'path';
 import { Request } from 'express';
 
-// Dosyaları diske kaydetmek için bir `diskStorage` yapılandırması tanımlayın
-const storage: StorageEngine = multer.diskStorage({
+const storage = multer.diskStorage({
     filename: (req: Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) => {
         const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
         cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));

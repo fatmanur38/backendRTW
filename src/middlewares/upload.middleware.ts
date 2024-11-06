2f743a204db94ba0853bc5358f51ccb3caf70a08
Fatmanur Ergen <fatmanurergen38@gmail.com>
2024-11-06 14:50:32 +0300

    fixed upload middle

--- src/middlewares/upload.middleware.ts
+++ src/middlewares/upload.middleware.ts
@@ -1,14 +1,16 @@
-// src/middlewares/upload.middleware.ts
+import { Request } from 'express';
 import multer from 'multer';
 import path from 'path';
-import { Request } from 'express';
+
+interface MulterRequest extends Request {
+  file: Express.Multer.File;
+}
 
 const storage = multer.diskStorage({
-    filename: (req: Request, file: Express.Multer.File, cb: (error: Error | null, filename: string) => void) => {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
-    },
+  filename: (req: MulterRequest, file: Express.Multer.File, cb) => {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  },
 });
 
-// Middleware olarak `multer` yapılandırmasını dışa aktarın
 export const uploadMiddleware = multer({ storage });

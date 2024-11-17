030f3bc3f2aa56b2d3e9076f7737268c3e7934ad
Fatmanur Ergen <fatmanurergen38@gmail.com>
2024-11-17 19:25:40 +0300

    fixed cors

--- src/app.ts
+++ src/app.ts
@@ -19,20 +19,27 @@ dotenv.config();
 
 const app: Application = express();
 
-const allowedOrigins = [`${process.env.FRONTEND_URL}`, `${process.env.USER_FRONTEND_URL}`]; // İki frontend portu
+//const allowedOrigins = [`${process.env.FRONTEND_URL}`, `${process.env.USER_FRONTEND_URL}`]; // İki frontend portu
 
 app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  // origin: (origin, callback) => {
+  //   if (!origin || allowedOrigins.includes(origin)) {
+  //     callback(null, true);
+  //   } else {
+  //     callback(new Error('Not allowed by CORS'));
+  //   }
+  // },
+  // credentials: true,
+  // methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  // allowedHeaders: ['Content-Type', 'Authorization'],
+  origin: [process.env.FRONTEND_URL || 'http://localhost:5173' , 'http://localhost:5173'],
+  methods: 'GET,POST,PUT,DELETE',
+  allowedHeaders: 'Content-Type, Authorization',
+  credentials: true, // Bu seçenek, çerezleri etkinleştirir
 }));
+
+
+// Enable CORS
 
 // Database Connection
 mongoose
